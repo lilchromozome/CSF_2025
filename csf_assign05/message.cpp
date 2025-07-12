22fe4a8efa53f36bc7dd2b7edcaa925227fd5e50
SichenDeng <dengsichen77@gmail.com>
2025-07-12 00:22:35 -0400

    message.cpp

--- csf_assign05/message.cpp
+++ csf_assign05/message.cpp
@@ -28,6 +28,10 @@ Message::~Message()
 Message &Message::operator=( const Message &rhs )
 {
   // TODO: implement
+  if (this != &rhs) {
+    m_message_type = rhs.m_message_type;
+    m_args = rhs.m_args;
+  }
   return *this;
 }
 
@@ -44,30 +48,46 @@ void Message::set_message_type(MessageType message_type)
 std::string Message::get_username() const
 {
   // TODO: implement
+  if(m_message_type == MessageType::LOGIN && m_args.size()==1){
+    return m_args[0];
+  }
   return "";
 }
 
 std::string Message::get_table() const
 {
   // TODO: implement
+  if ((m_message_type == MessageType::CREATE && m_args.size() == 1)
+    || ((m_message_type == MessageType::SET || m_message_type == MessageType::GET) && m_args.size() == 2)){
+      return m_args[0];
+  }
   return "";
 }
 
 std::string Message::get_key() const
 {
   // TODO: implement
+  if ((m_message_type == MessageType::SET || m_message_type == MessageType::GET) && m_args.size() >= 2){
+    return m_args[1];
+  }
   return "";
 }
 
 std::string Message::get_value() const
 {
   // TODO: implement
+  if ((m_message_type == MessageType::PUSH || m_message_type == MessageType::DATA) && m_args == 1){
+    return m_args[0];
+  }
   return "";
 }
 
 std::string Message::get_quoted_text() const
 {
   // TODO: implement
+  if ((m_message_type == MessageType::FAILED || m_message_type == MessageType::ERROR) && m_args == 1){
+    return m_args[0];
+  }
   return "";
 }
 
@@ -79,5 +99,33 @@ void Message::push_arg( const std::string &arg )
 bool Message::is_valid() const
 {
   // TODO: implement
-  return true;
+  switch (m_message_type) {
+    // 1 args
+    case MessageType::LOGIN:
+    case MessageType::PUSH:
+    case MessageType::CREATE:
+    case MessageType::FAILED:
+    case MessageType::ERROR:
+    case MessageType::DATA:
+      return m_args.size()==1;
+
+    // 2 args
+    case MessageType::SET:
+    case MessageType::GET:
+      return m_args.size() == 2;
+
+    // 0 args
+    case MessageType::POP:
+    case MessageType::TOP:
+    case MessageType::ADD:
+    case MessageType::MUL:
+    case MessageType::SUB:
+    case MessageType::DIV:
+    case MessageType::BEGIN:
+    case MessageType::COMMIT:
+    case MessageType::BYE:
+    case MessageType::OK:
+      return m_args.empty();
+  }
+  return false;
 }
