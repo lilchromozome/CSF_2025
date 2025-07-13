819de51fe1c01c3ca38fd9e767bb3439c8046500
SichenDeng <dengsichen77@gmail.com>
2025-07-13 00:35:21 -0400

    message_serialization

--- csf_assign05/message_serialization.cpp
+++ csf_assign05/message_serialization.cpp
@@ -87,4 +87,67 @@ void MessageSerialization::encode( const Message &msg, std::string &encoded_msg 
 void MessageSerialization::decode( const std::string &encoded_msg_, Message &msg )
 {
   // TODO: implement
+  std::string encoded_msg = encoded_msg_;
+  if (!encoded_msg.empty() && encoded_msg.back() == '\n') {
+    encoded_msg.pop_back(); // Remove trailing newline
+  }
+  std::istringstream iss(encoded_msg);
+  std::string token;
+  std::vector<std::string> tokens;
+
+  // if token starts with "", it is a quoted string
+  while (iss >> token) {
+    if (token[0] == '"') {
+      // Handle quoted strings
+      std::string quoted_token = token;
+      while (!quoted_token.empty() && quoted_token.back() != '"') {
+        std::string next_token;
+        if (!(iss >> next_token)) {
+          break;
+        }
+        quoted_token += ' ' + next_token;
+      }
+      quoted_token = quoted_token.substr(1, quoted_token.size() - 2); // Remove quotes
+      tokens.push_back(quoted_token);
+    } else {
+      tokens.push_back(token);
+    }
+  }
+
+  static const std::map<std::string, MessageType> type_map = {
+    {"LOGIN", MessageType::LOGIN},
+    {"CREATE", MessageType::CREATE},
+    {"PUSH", MessageType::PUSH},
+    {"POP", MessageType::POP},
+    {"TOP", MessageType::TOP},
+    {"SET", MessageType::SET},
+    {"GET", MessageType::GET},
+    {"ADD", MessageType::ADD},
+    {"SUB", MessageType::SUB},
+    {"MUL", MessageType::MUL},
+    {"DIV", MessageType::DIV},
+    {"BEGIN", MessageType::BEGIN},
+    {"COMMIT", MessageType::COMMIT},
+    {"BYE", MessageType::BYE},
+    {"OK", MessageType::OK},
+    {"FAILED", MessageType::FAILED},
+    {"ERROR", MessageType::ERROR},
+    {"DATA", MessageType::DATA}
+  };
+
+  if (tokens.empty())
+  {
+    throw SerializationException("Empty message received");
+  }
+
+  auto it = type_map.find(tokens[0]);
+  if (it == type_map.end())
+  {
+    throw SerializationException("Unknown message type: " + tokens[0]);
+  }
+  msg.set_message_type(it->second);
+  for (size_t i = 1; i < tokens.size(); ++i)
+  {
+    msg.push_arg(tokens[i]);
+  }
 }
