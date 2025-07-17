b27311077b737df4d3d0d8a6361f3ba86cfff413
lilchromozome <williamli5709@gmail.com>
2025-07-16 20:32:21 -0400

    value_stack passed

--- csf_assign05/value_stack.cpp
+++ csf_assign05/value_stack.cpp
@@ -13,19 +13,29 @@ ValueStack::~ValueStack()
 bool ValueStack::is_empty() const
 {
   // TODO: implement
+  return stack.empty();
 }
 
 void ValueStack::push( const std::string &value )
 {
   // TODO: implement
+  stack.push_back(value);
 }
 
 std::string ValueStack::get_top() const
 {
   // TODO: implement
+  if (stack.empty()){
+    throw OperationException("ValueStack is empty");
+  }
+  return stack.back();
 }
 
 void ValueStack::pop()
 {
   // TODO: implement
+  if (stack.empty()){
+    throw OperationException("ValueStack is empty");
+  }
+  return stack.pop_back();
 }
